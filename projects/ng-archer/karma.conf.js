cb73be56e7872e78e9aba7d8cc8c201b38d82010
Sergey Pashnin <sspinit88@gmail.com>
2020-12-02 22:31:39 +0300

    - настроил карму для ie11, ff, chrome; - настроил ng для работы с ie11;

--- projects/ng-archer/karma.conf.js
+++ projects/ng-archer/karma.conf.js
@@ -8,6 +8,8 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
+      require('karma-firefox-launcher'),
+      require('karma-ie-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
@@ -31,7 +33,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['Chrome', 'Firefox', 'IE'],
     singleRun: false,
     restartOnFileChange: true
   });
